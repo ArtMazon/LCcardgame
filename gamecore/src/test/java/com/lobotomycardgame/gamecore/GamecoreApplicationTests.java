5374f93938b148798764b1dd3a8fa758887eddff
ArtMaz <49465717+ArtMazon@users.noreply.github.com>
2024-03-13 23:41:53 -0600

    First commitment

---
+++ gamecore/src/test/java/com/lobotomycardgame/gamecore/GamecoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobotomycardgame.gamecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
